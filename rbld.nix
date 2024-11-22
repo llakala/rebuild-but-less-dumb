b7d9ff84a6ccb5b4f0a4264685f9150299e733fc
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-11-22 16:39:27 -0500

    treewide: move scripts into `.sh` files rather than inside nix files
    Integrated scripts in Nix is very nice, and tree-sitter gives me syntax
    highlighting. But it does NOT give me language servers, which I love. So,
    we're living the `.sh` life again.

--- rbld.nix
+++ rbld.nix
@@ -23,39 +23,5 @@ in pkgs.writeShellApplication
     "pipefail"
     "errtrace" # -E
   ];
-  text =
-  ''
-    shopt -s inherit_errexit
-
-    directory="''${FLAKE:-/etc/nixos}" # Override default config directory value with $FLAKE
-
-    while getopts ":d:" opt; do # Or, if you just need to override the directory once, use `-d`
-      case $opt in
-        d)
-          directory=$OPTARG
-          ;;
-        \?) # Undefined option like -q
-          echo "Invalid option: -$OPTARG" >&2
-          exit 1
-          ;;
-        :) # Setting -d without an argument
-          echo "Option -$OPTARG requires an argument." >&2
-          exit 1
-          ;;
-      esac
-    done
-
-    hue "$directory"
-    cd "$directory"
-
-    git add -AN # Adds the existence of any new files, but not their contents
-
-    sudo -v || exit # Rather than having to verify sudo during rebuild, we do it before. works as long as rebuild is <5 minutes
-    nixos-rebuild switch \
-      --use-remote-sudo --fast \
-      --log-format internal-json \
-      --flake "$directory" \
-      |& nom --json || exit 1
-
-    '';
+  text = builtins.readFile ./scripts/rbld.sh;
 }
---
+++ scripts/unify.sh
@@ -0,0 +1,139 @@
+shopt -s inherit_errexit
+
+# Use environment variables if they're overriding the default values
+DIRECTORY="${FLAKE:-/etc/nixos}" # Directory that your NixOS config is located in
+IMPORTANT_INPUTS="${INPUTS_TRIGGERING_REBUILD:-nixpkgs rebuild-but-less-dumb}" # Trigger `nix flake update` if one of these inputs is updated
+FLAKE_COMMIT_MESSAGE="${FLAKE_COMMIT_MESSAGE:-flake: update flake.lock}" # The commit message to use for flake.lock updates
+PRIMARY_BRANCHES="${PRIMARY_BRANCHES:-main master}" # branches that are allowed to have flake.lock changes commited to
+
+
+while getopts ":d:i:c:p:" opt; do # Override default values without setting a permanent custom default via environment vars
+  case $opt in
+    d)
+      DIRECTORY=$OPTARG
+      ;;
+    i)
+      IMPORTANT_INPUTS=$OPTARG
+      ;;
+    c)
+      FLAKE_COMMIT_MESSAGE=$OPTARG
+      ;;
+    p)
+      PRIMARY_BRANCHES=$OPTARG
+      ;;
+    \?) # Undefined option like -q
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :) # using an argument without passing something (ex: `unify -d`)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+
+sum_all_revisions() # Call get_revision_time for each input in IMPORTANT_INPUTS
+{
+  sum=0
+  for input in $IMPORTANT_INPUTS; do
+    time=$(
+      < flake.lock jq --arg input "$input" \
+      '.nodes.[$input].locked.lastModified'
+    )
+    if [[ $time == "null" ]]; then
+      echo "Input \`$input\` wasn't found in the flake.lock. Maybe you named it something else, or made a typo?"
+      return 1
+    fi
+
+    sum=$((sum + time))
+  done
+  echo "$sum" # Returns value of sum
+}
+
+return_to_secondary() # Called as a trap so we return from main/master to whatever branch the user was on
+{
+  current_branch=$(git branch --show-current)
+  if [[ $current_branch != "$previous_branch" ]]; then
+    git switch --quiet "$previous_branch"
+  fi
+  git restore flake.lock
+}
+
+on_primary_branch() # Return whether we're on one of the branches stored in PRIMARY_BRANCHES
+{
+  local current_branch=$1
+
+  for primary_branch in $PRIMARY_BRANCHES; do
+
+    if [[ $current_branch == "$primary_branch" ]]; then
+      return 0
+    fi
+
+  done
+  return 1
+}
+
+switch_to_primary() # Return 1 if we couldn't find a primary branch to switch into
+{
+  for branch in $PRIMARY_BRANCHES; do
+    if git rev-parse --verify "$branch" > /dev/null 2>&1; then
+      git switch --quiet "$branch"
+      return 0
+    fi
+  done
+
+  return 1
+}
+
+hue "$DIRECTORY" # Check that the passed directory was valid
+cd "$DIRECTORY"
+
+previous_branch=$(git branch --show-current) # Only set this *after* entering $DIRECTORY
+
+if [[ -n $(git status --porcelain) ]] && ! on_primary_branch "$previous_branch"; then # Exit early if we're not in primary branch and have uncommited changes
+  echo "You have uncommited changes in your current branch \`$previous_branch\`."
+  echo "Unify only updates flake inputs on the primary branch, as it's likely what you meant to do."
+  echo "You can specify the primary branch/branches to be swapped to like this:"
+  echo "\`unify -p \"main master\"\`"
+  echo "If your working tree is clean, Unify will then switch to a primary branch automatically."
+  exit 1
+fi
+
+
+if switch_to_primary; then
+  trap return_to_secondary EXIT # When script ends or is interrupted, swap back to the branch the user was on before
+else
+  echo "You provided the primary branches \`$PRIMARY_BRANCHES\` to be switched to automatically."
+  echo "However, none of these branches were found in directory \`$DIRECTORY\`."
+  exit 1
+fi
+
+if ! old_time=$(sum_all_revisions); then
+  echo "$old_time" # We return the error message from the function directly
+  exit 1
+fi
+nix flake update
+new_time=$(sum_all_revisions) # We only check for errors once, and don't check again here. Should be fine (hopefully).
+
+echo "Old time: $old_time" # Logs for debugging
+echo "New time: $new_time"
+
+if [[ $old_time == "$new_time" ]]; then
+  echo "No important updates to flake.lock, so skipping rebuild"
+
+  echo "Undoing flake.lock changes."
+  git restore flake.lock
+
+  exit 0
+fi
+
+rbld -d "$DIRECTORY"
+git commit --quiet --message "$FLAKE_COMMIT_MESSAGE" flake.lock
+
+if ! git ls-remote origin --quiet; then # For when internet is spotty
+  echo "Can't reach the remote repo to push. Try pushing again later."
+  exit 1
+fi
+
+echo "Connection found, pushing."
+git push --quiet
