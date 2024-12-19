602cabfc5ff360711bab9d1faeb57641514c6416
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-12-18 19:02:35 -0500

    fuiska: access all needed flake.lock values
    Definitely ways to clean this code up, but we're going for functionality
    first.

--- scripts/fuiska.sh
+++ scripts/fuiska.sh
@@ -4,4 +4,25 @@ directory="${RBLD_DIRECTORY:-/etc/nixos}"
 
 hue "$directory"
 cd "$directory"
-ls
+
+contents=$(cat flake.lock)
+inputs=$(echo "$contents" | jq -r ".nodes.root.inputs | keys[]")
+
+for input in $inputs; do
+
+  data=$(echo "$contents" | jq -r --arg input "$input" '.nodes.[$input]')
+
+  type=$(echo "$data" | jq -r ".original.type")
+  owner=$(echo "$data" | jq -r ".original.owner")
+  repo=$(echo "$data" | jq -r ".original.repo")
+  ref=$(echo "$data" | jq -r ".original.ref") # TODO: HANDLE NULL
+  hash=$(echo "$data" | jq -r ".locked.rev")
+
+  echo "$type"
+  echo "$owner"
+  echo "$repo"
+  echo "$ref"
+  echo "$hash"
+  echo ""
+
+done
