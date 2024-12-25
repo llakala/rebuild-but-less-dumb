36cd8cc5ec76c4f9fed2c5160c5f16126a2c897b
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2024-12-24 22:54:57 -0500

    fuiska: move parallelizable content into function
    For a future commit with parallelization. It's literally just adding two
    lines, but I still prefer to split it up.

--- scripts/fuiska.sh
+++ scripts/fuiska.sh
@@ -6,19 +6,17 @@ hue "$directory"
 cd "$directory"
 
 contents=$(cat flake.lock)
-inputs=$(echo "$contents" | jq -r ".nodes.root.inputs | keys[]")
 
-echo "Whether each input needs updating:"
-
-for input in $inputs; do
+func()
+{
+  input=$1
   data=$(echo "$contents" | jq -r --arg input "$input" '.nodes.[$input]')
-
 
   # The input will never update if it points to a specific commit hash
   evergreen=$(echo "$data" | jq -r "if .original.rev then 0 else 1 end")
 
   if [ "$evergreen" == 0 ]; then
-    continue
+    exit 0 # equivalent of `continue` within a subshell
   fi
 
 
@@ -26,17 +24,17 @@ for input in $inputs; do
   branch=$(echo "$data" | jq -r 'if .original.ref then .original.ref else "HEAD" end')
 
   oldHash=$(echo "$data" | jq -r ".locked.rev")
-  newHash=$(git ls-remote --branches "$url" "$branch" | cut -f1)
-
-  echo "input: $input"
-  echo "url: $url"
-  echo "branch: $branch"
+  newHash=$(git ls-remote --branches --r "$url" "$branch" | cut -f1)
 
   if [ "$oldHash" != "$newHash" ]; then
-    echo "needs update: YES"
-  else
-    echo "needs update: NO"
+    echo "$input"
   fi
-  echo ""
+}
 
+inputs=$(echo "$contents" | jq -r ".nodes.root.inputs | keys[]")
+
+echo "Inputs that need updating:"
+
+for i in $inputs; do
+  func "$i"
 done
