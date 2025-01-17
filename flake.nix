67ee739c4aa98b8be2d73737a7def228d15e39ef
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-01-17 16:21:52 -0500

    llakaLib: access with system parameter

--- flake.nix
+++ flake.nix
@@ -9,33 +9,50 @@
 
   outputs = { self, nixpkgs, ... } @ inputs:
   let
+    lib = nixpkgs.lib;
+
+    # The "normal" systems. If it ever doesn't work with one of these, or you want me
+    # to add a system, let me know!
+    supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64_linux" "aarch64-darwin" ];
+
+    forAllSystems = function: lib.genAttrs
+      supportedSystems
+      (system: function nixpkgs.legacyPackages.${system});
+
     # My custom lib functions, declared in another repo so I can use them across projects
-    llakaLib = inputs.llakaLib.lib;
+    # Some of them require `pkgs`, so this function gives you a `llakaLib` instance from
+    # `fullLib`, which includes system-dependent functions.
+    mkLlakaLib = system: inputs.llakaLib.fullLib.${system};
 
   in
   {
-    packages = llakaLib.forAllSystems
+    packages = forAllSystems
     (
-      pkgs: llakaLib.collectDirectoryPackages
+      pkgs: let llakaLib = mkLlakaLib pkgs.system;
+      in llakaLib.collectDirectoryPackages
       {
         inherit pkgs;
         directory = ./packages;
+
+        extras = { inherit llakaLib; }; # Lets the packages rely on llakaLib
       }
     );
 
-    devShells = llakaLib.forAllSystems (pkgs:
-    {
-      default = pkgs.mkShell
+    devShells = forAllSystems
+    (pkgs:
       {
-        packages = with self.packages.${pkgs.system};
-        [
-          rbld
-          unify
-          hue # You don't need to install this manually, it's a dependency of the others. I just use install it manually for debugging
-          fuiska
-        ];
-      };
-    });
+        default = pkgs.mkShell
+        {
+          packages = with self.packages.${pkgs.system};
+          [
+            rbld
+            unify
+            hue # You don't need to install this manually, it's a dependency of the others. I just use install it manually for debugging
+            fuiska
+          ];
+        };
+      }
+    );
 
   };
 }
